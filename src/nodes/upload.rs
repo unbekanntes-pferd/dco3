716e024f2ea46e7eb6d90a172a86304b028394b7
Octavio Simone <70800577+unbekanntes-pferd@users.noreply.github.com>
2023-10-18 20:06:42 +0200

    fix setting file key (wrong file id)

--- src/nodes/upload.rs
+++ src/nodes/upload.rs
@@ -734,7 +734,7 @@ impl<R: AsyncRead + Sync + Send + Unpin + 'static> UploadInternal<R> for Dracoon
                                 public_key,
                             )?;
                             let set_key_req =
-                                UserFileKeySetRequest::new(file_id, user_id, file_key);
+                                UserFileKeySetRequest::new(user_id, file_id, file_key);
                             Ok(set_key_req)
                         })
                         .collect::<Vec<_>>();
